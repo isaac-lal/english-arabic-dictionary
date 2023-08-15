ed9a6f4cac4cf0b487dfc8b4050726181e04b147
Isaac Lal <isaaclal124@gmail.com>
2023-08-15 19:16:37 -0400

    initial production completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,17 @@
 import React from 'react';
-import { useState } from 'react';
 import './index.css';
-import Table from './Table';
 import WordsData from './data.json';
+import Search from './Search';
 
 function App() {
-  const [query, setQuery] = useState('');
-
-  const search = (words) => {
-    return words.filter(
-      (word) =>
-        word.english.toLowerCase().includes(query) ||
-        word.arabic.toLowerCase().includes(query)
-    );
-  };
-
   return (
-    <div className='container'>
-      <input
-        type='text'
-        placeholder='Search a word in English or Arabic'
-        onChange={(e) => {
-          setQuery(e.target.value.toLowerCase());
-        }}
+    <div>
+      <h1>English to Arabic Translator</h1>
+      <h1>مترجم عربي انجليزي</h1>
+      <Search
+        placeholder='Search يبحث'
+        data={WordsData}
       />
-      <Table words={search(WordsData)} />
     </div>
   );
 }
---
+++ src/Search.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import './index.css';
+
+function SearchBar({ placeholder, data }) {
+  const [filteredData, setFilteredData] = useState([]);
+  const [wordEntered, setWordEntered] = useState('');
+
+  const handleFilter = (e) => {
+    const searchWord = e.target.value;
+    setWordEntered(searchWord);
+
+    const newFilter = data.filter((words) => {
+      return (
+        words.english.toLowerCase().includes(searchWord) ||
+        words.arabic.toLowerCase().includes(searchWord)
+      );
+    });
+
+    if (searchWord === '') {
+      setFilteredData([]);
+    } else {
+      setFilteredData(newFilter);
+    }
+  };
+
+  return (
+    <div className='search__container'>
+      <div className='search__styles'>
+        <input
+          type='text'
+          placeholder={placeholder}
+          value={wordEntered}
+          onChange={handleFilter}
+        />
+      </div>
+      {filteredData.length != 0 && (
+        <div>
+          <table>
+            <tbody>
+              <tr>
+                <th>English</th>
+                <th>عربي</th>
+              </tr>
+              {filteredData.slice(0, 5).map((word) => (
+                <tr>
+                  <td>{word.english}</td>
+                  <td>{word.arabic}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default SearchBar;
